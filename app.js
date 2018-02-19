e2c77e228e3fe87d3365201a9148e28304cf33f6
Jeff Vega <jeff@intellibliss.com>
2018-02-19 12:12:56 -0800

    added birth year within its own function

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function yearOfBirth(age){
-
-}
-
-
-function whoIAm(name,age){
-    var yearOfBirth = 2018 - age;
-console.log(`Hi, my name is ${name} and I'm ${age} years old`)
-/* ----If wanted to code in our Birth Year 
-console.log(`I was born in ${yearOfBirth}`)
-*/
-};
-whoIAm("jeff",20);
+    return 2018 - age ;
+    }
+    
+    
+    function whoIAm(name,age){
+        var yob = yearOfBirth(20)
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`)
+    /* ----If wanted to code in our Birth Year 
+    console.log(`I was born in ${yearOfBirth}`)
+    */
+    console.log(yob)
+    };
+    whoIAm("jeff",20);
