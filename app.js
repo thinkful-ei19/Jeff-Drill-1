fcdbd6f4f365ffdb0664cab2e799fd16f7f033e8
Jeff Vega <jeff@intellibliss.com>
2018-02-19 13:16:30 -0800

    make sure that strings and negative number will become errors

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function yearOfBirth(age){
-    return 2018 - age ;
+function yearOfBirth(age1){
+   if(age1<0){
+       throw new Error("Age can not be negative")
+   }
+   if(isNaN(age1)){
+       throw new Error("Age Must Be A Vaild Number")
+   }
+    return 2018 - age1 ;
+
     }
     
     
     function whoIAm(name,age){
-        var yob = yearOfBirth(20)
+        var yob = yearOfBirth(age)
     console.log(`Hi, my name is ${name} and I'm ${age} years old`)
     /* ----If wanted to code in our Birth Year 
     console.log(`I was born in ${yearOfBirth}`)
     */
     console.log(yob)
     };
-    whoIAm("jeff",20);
+    whoIAm("jeff","twenty five");
